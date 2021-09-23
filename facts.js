6defbf81268fe59b192af813c29f73405404a2e0
Ganesh <ganuofficial2795@gmail.com>
2021-09-23 20:05:45 +0530

    Facts

--- facts.js
+++ facts.js
@@ -1,1 +1,22 @@
 console.log("Number Facts 🚀");
+
+let num = document.querySelector('#num');
+let showFact = document.querySelector('#showFact');
+let types = ['trivia','math'];
+
+num.addEventListener('input',()=>{
+
+    let type = types[Math.floor(Math.random()*2)];
+    let number = Number(num.value); //Default is 0
+
+    let url = `http://numbersapi.com/${number}/${type}`;
+    
+    console.log(url);
+    fetch(url).then(res=>{
+        
+        return res.text();
+    }).then(data=>{
+        showFact.innerHTML = data
+    })
+
+});
